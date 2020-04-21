37c734afe865713acf79de83491d2b559867c8a2
suresh-krish <sureshvinz66@msitprogram.net>
2020-04-21 12:55:10 +0530

    registration

--- project1/application.py
+++ project1/application.py
@@ -1,17 +1,26 @@
 import os
-
-from flask import Flask, session
+from flask import Flask, session,request,render_template,flash,logging,redirect,url_for
 from flask_session import Session
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-from flask.templating import render_template
-from flask import request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime as dt
+from models import User,db
+
 
 app = Flask(__name__)
 
-# Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key="email"
+
+db.init_app(app)
+def main():
+    db.create_all()
+
+# Check for environment variable
+
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
@@ -19,19 +28,36 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+# engine = create_engine(os.getenv("DATABASE_URL"))
+# db = scoped_session(sessionmaker(bind=engine))
+
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/register" , methods=["GET","post"])
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == "GET":
-        return "PLEASE SUBMIT THE FORM"
-    else:
-        name = request.form.get("name")
-        return render_template("register.html", name=name)
 
+    if (request.method == "POST"):
+        mail = request.form['name']
+        passw = request.form['pwd']
+
+        # count = User.query.filter_by(email=mail).count()
+        # print (count)
+        try:
+            register = User(email = mail, password = passw)
+            db.session.add(register)
+            db.session.commit()
+            a=User.query.all()
+            print(a)
+            return render_template("register.html",name=mail)
+        except:
+            error="You have already registered with this email"
+            return render_template("register.html",message=error)
+    return render_template("register.html")
+
+@app.route("/login.html",methods=["GET","POST"])
+def login():
+    return render_template("login.html")
