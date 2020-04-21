2ae69a19140f06f8a48b1c1bb97c819927e4c832
suresh-krish <sureshvinz66@msitprogram.net>
2020-04-21 13:33:16 +0530

    project1

--- project1/application.py
+++ project1/application.py
@@ -61,3 +61,44 @@ def register():
 @app.route("/login.html",methods=["GET","POST"])
 def login():
     return render_template("login.html")
+@app.route("/admin")
+def admin():
+    users=User.query.all()
+    return render_template ("admin.html",data=users)
+
+@app.route("/auth", methods=["GET","POST"])
+def auth():
+    if request.method=="POST":
+        mail = request.form.get("name") # i will get my email entered in the webpage
+        passw = request.form.get("pwd")
+        u = User.query.get(mail)
+        #select * from user where email==mail  ===> email,password,timestamp
+        if u != None:
+            # print (u)
+            # print (u.password)
+            # print (passw,"====> entered password")
+
+            if passw == u.password :
+            #     print ("your mail id is found")
+                session["email"]=mail
+                return render_template("account.html")
+            else:
+                return "invalid password"
+        else:
+            return "No account associated with this password"
+
+@app.route("/search",methods=["GET"])
+def search():
+    if session["email"] == None:
+        return redirect(url_for("/logout"))
+    else:
+        return "Maintained successfully"
+
+@app.route("/logout",methods=["GET","POST"])
+def logout():
+    session["email"]=None
+    return redirect("/register")
+
+# if __name__ == "__main__":
+with app.app_context():
+    main()
